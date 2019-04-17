c02ae5da50a53d3de31d6f03367e5bbfc58dad66
Shamimi Amran <amran.shamimi@gmail.com>
2019-04-17 21:57:49 +0800

    event

---
+++ Zealous/Models/Event.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Zealous.Models
+{
+    public class Event
+    {
+        public int Id { get; set; }
+        public int EventName { get; set; }
+        public int Description { get; set; }
+    }
+}
