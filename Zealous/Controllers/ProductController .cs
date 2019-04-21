e9f64fbd99896edb9711bcff8799731bc0d00c5f
Shamimi Amran <amran.shamimi@gmail.com>
2019-04-22 00:18:24 +0800

    mimi

---
+++ Zealous/Controllers/ProductController .cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Zealous.Models;
+
+namespace Zealous.Controllers
+{
+    public class ProdutController : ZealousController
+    {
+
+       //GET:
+       
+
+    }
+}
