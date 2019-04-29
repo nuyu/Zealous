eb98a345f4fa9ffb4ce2d37ec749f619e874e7a6
Hisham Alwanni <hisham.alwanny@gmail.com>
2019-04-30 00:28:00 +0800

    EventTrackiing Model

--- Zealous/Models/ZealousContext.cs
+++ Zealous/Models/ZealousContext.cs
@@ -13,6 +13,7 @@ namespace Zealous.Models
         public DbSet<Payment> Payments { get; set; }
         public DbSet<Event> Events { get; set; }
         public DbSet<Product> Products { get; set; }
+        public DbSet<EventTracking> EventTrackings { get; set; }
 
         public ZealousContext()
             : base("DefaultConnection")
